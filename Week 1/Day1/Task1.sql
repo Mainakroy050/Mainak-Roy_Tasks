170cf12b5f46b1dd784557a70b54270afcd5474d
Mainak Roy <mainakroy050@gmail.com>
2024-07-30 10:17:31 +0530

    Update Task1.sql

--- Week 1/Day1/Task1.sql
+++ Week 1/Day1/Task1.sql
@@ -5,7 +5,7 @@ CREATE TABLE students(
 s_id INT PRIMARY KEY,
 s_name VARCHAR(20),s_age INT);
 
-INSERT INTO students (s_id,s_name_s_age) VALUES
+INSERT INTO students (s_id,s_name,s_age) VALUES
 (1,"Ram",12),
 (2,"Shamam",21);
 
