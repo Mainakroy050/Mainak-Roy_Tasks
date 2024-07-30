252f06e3d8bcd8f26dcad246aa44ea7664585126
Mainak Roy <mainakroy050@gmail.com>
2024-07-30 10:07:54 +0530

    Update Task 2.sql

--- Week 1/Day1/Task 2.sql
+++ Week 1/Day1/Task 2.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE school_management;
 USE school_management;
-
-CREATE TABLE school_management.students (
+CREATE TABLE students (
     student_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
@@ -9,7 +8,7 @@ CREATE TABLE school_management.students (
     phone_number VARCHAR(15),
     date_of_birth DATE
 );
-CREATE TABLE school_management.courses (
+CREATE TABLE courses (
     course_id INT PRIMARY KEY AUTO_INCREMENT,
     course_name VARCHAR(100),
     course_description TEXT,
@@ -22,22 +21,22 @@ ADD COLUMN grade VARCHAR(2);
 ALTER TABLE school_management.students
 MODIFY COLUMN phone_number VARCHAR(15);
 
-INSERT INTO school_management.students (first_name, last_name, email, phone_number, date_of_birth, grade)
+INSERT INTO students (first_name, last_name, email, phone_number, date_of_birth, grade)
 VALUES 
-('John', 'Doe', 'john.doe@example.com', '123-456-7890', '2000-01-01', 'A'),
-('Jane', 'Smith', 'jane.smith@example.com', '234-567-8901', '2001-02-02', 'B'),
-('Alice', 'Johnson', 'alice.johnson@example.com', '345-678-9012', '2002-03-03', 'A');
+('Pradhan', 'dads', 'john.doe@example.com', '4545458781', '2000-01-01', 'A'),
+('Pratyush', 'samanta', 'jane.smith@example.com', '566456455', '2001-02-02', 'B'),
+('Rahul', 'Mohanty', 'alice.jo@le.com', '6545646545', '2002-03-03', 'A');
 
-INSERT INTO school_management.courses (course_name, course_description, credits)
+INSERT INTO courses (course_name, course_description, credits)
 VALUES 
 ('Mathematics', 'Basic mathematics course', 3),
-('English', 'English language and literature', 4),
-('History', 'World history overview', 3),
-('Biology', 'Introduction to biology', 4),
-('Computer Science', 'Fundamentals of computer science', 3);
+('English', 'English language', 4),
+('History', 'World history ', 3),
+('Biology', 'biology', 4),
+('Computer Science', 'computer science', 3);
 
-SELECT first_name, last_name, email FROM school_management.students;
+SELECT first_name, last_name, email FROM students;
 
-DROP TABLE school_management.students;
+DROP TABLE students;
 
 DROP DATABASE school_management;
